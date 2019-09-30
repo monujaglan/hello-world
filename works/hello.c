901a16b4db471487994cbd54492f9811afd194bb
user <jaglanmonu1998@gmail.com>
2019-09-30 11:56:51 +0300

    20190930_11_56

---
+++ works/hello.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+// this library should be added for printf for work
+void main()
+{
+//Hello world!; //absoutely wrong!
+"Hello world!"; //working, but text doesn't appear on screen
+printf("Hello world!\n");
+}
